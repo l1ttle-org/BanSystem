0d703ed806af5fca0f5fb6f0b5bf19b30079518e
l1ttleO <61277953+l1ttleO@users.noreply.github.com>
2021-01-15 23:43:16 +0500

    Remove lastBanID and lastBlacklistID (testing reasons)

--- src/main/java/io/github/l1ttle_org/bansystem/BanSystem.java
+++ src/main/java/io/github/l1ttle_org/bansystem/BanSystem.java
@@ -26,16 +26,16 @@ public final class BanSystem extends JavaPlugin implements Listener {
             PaperLib.suggestPaper(this);
         }
         saveDefaultConfig();
-        FileConfiguration dataConfig = getDataConfig();
-        if (dataConfig.get("lastBanID") == null) {
-            dataConfig.set("lastBanID", 1);
-            saveDataConfig();
-        }
-        if (dataConfig.get("lastBlacklistID") == null) {
-            dataConfig = getDataConfig();
-            dataConfig.set("lastBlacklistID", 1);
-            saveDataConfig();
-        }
+        //FileConfiguration dataConfig = getDataConfig();
+        //if (dataConfig.get("lastBanID") == null) {
+        //    dataConfig.set("lastBanID", 1);
+        //    saveDataConfig();
+        //}
+        //if (dataConfig.get("lastBlacklistID") == null) {
+        //    dataConfig = getDataConfig();
+        //    dataConfig.set("lastBlacklistID", 1);
+        //    saveDataConfig();
+        //}
         getCommand("ban").setExecutor(new CommandBan(this));
         getCommand("blacklist").setExecutor(new CommandBlacklist(this));
         getCommand("kick").setExecutor(new CommandKick());
